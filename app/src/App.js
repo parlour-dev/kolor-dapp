c21d68cb01b4c3c5a4d77bdc8af249982385a5a7
chet2chet <karolproksa@gmail.com>
2021-09-10 19:24:40 +0200

    Implemented @KubaMach's component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import PostImage from "../src/components/PostImage/PostImage"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>eluwina</p>
+      <PostImage />
     </div>
   );
 }
