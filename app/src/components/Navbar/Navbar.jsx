ebdb2a346e28311f4924ab0c1f6fd15197e9acb5
chet2chet <karolproksa@gmail.com>
2021-09-11 09:54:58 +0200

    Added the files.

---
+++ app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Navbar/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='navbar'>
+          <div className='navbar-button'>Create</div>
+          <div className='logo'>The Content Protocol</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
