e5295cfe3de60a80851f43f525b6b78e489a0395
chet2chet <karolproksa@gmail.com>
2021-09-11 09:53:04 +0200

    Created Navbar component and fixed some more PostImage component scaling.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
-import PostImage from "../src/components/PostImage/PostImage"
+import Navbar from "../src/components/Navbar/Navbar";
+import PostImage from "../src/components/PostImage/PostImage";
 // import PostText from "../src/components/PostText/PostText"
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <PostImage />
       {/* //<PostText />  */}
     </div>
