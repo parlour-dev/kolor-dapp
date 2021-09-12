2616416b8499721ff4851e9bc04fb8d818c76540
Kuba Machnicki <kubamach667@gmail.com>
2021-09-12 13:53:35 +0200

    postAdding

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "../src/components/Navbar/Navbar";
 import PostImage from "../src/components/PostImage/PostImage";
 import PostText from "../src/components/PostText/PostText";
 import "./App.css";
+import React, {useState} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,17 +14,22 @@ import {
 import CreateNewPost from "../src/components/CreateNewPost/CreateNewPost";
 
 function App() {
+
+
+  const [inputText, setInputText] = useState("");
+  const [posts, setPosts] = useState([]);
+
   return (
     <Router>
     <div className="App">
       <Navbar />
+     
       <Switch>
         <Route exact path='/'>
-          <PostImage />
-          <PostText />
+        {posts.map((post) =>(<PostText  text={post.text}/> ))}
         </Route>
         <Route path='/create'>
-          <CreateNewPost />
+          <CreateNewPost posts={posts} setPosts={setPosts} inputText={inputText} setInputText={setInputText} />
         </Route>
       </Switch>
     </div>
