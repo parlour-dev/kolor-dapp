9477724d7c0d4b44062e7667adff34ba755e4842
chet2chet <karolproksa@gmail.com>
2021-09-12 10:10:29 +0200

    Created CreateNewPost.jsx component.

--- app/src/components/CreateNewPost/CreateNewPost.jsx
+++ app/src/components/CreateNewPost/CreateNewPost.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import styles from "../CreateNewPost/CreateNewPost.css";
+import uploadImage from "../CreateNewPost/image.png";
+import submitArrow from "../CreateNewPost/arrow.png";
 
 const CreateNewPost = () => {
   return (
     <>
       <div className="createContainer">
         <div className="title">Create new post</div>
-        <div className="textField">
-          <input type="textarea" name="textValue" />
+        <div>
+          <form>
+            <textarea
+              className="textField"
+              placeholder="What's on your mind?"
+            ></textarea>
+          </form>
         </div>
-        <div className="uploadImage"></div>
-        <div className="submit"></div>
+        <div className="bottomButtons">
+          <div className="uploadImage">
+            <img src={uploadImage} alt="Upload Image" style={{width: "8vmax", marginTop: "1vmax",}} />
+          </div>
+          <div className="submit">
+            <img src={submitArrow} alt="Submit" style={{width: "6vmax", marginTop: "3vmax",}} />
+          </div>
+        </div>
       </div>
     </>
   );
