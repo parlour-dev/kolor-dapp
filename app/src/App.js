7fafc2e5b589431bd62fa506929b649a86cba37f
chet2chet <karolproksa@gmail.com>
2021-09-13 17:42:29 +0200

    Added working Log In with MetaMask button.

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import Navbar from "../src/components/Navbar/Navbar";
 import PostImage from "../src/components/PostImage/PostImage";
 import PostText from "../src/components/PostText/PostText";
 import "./App.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,25 +14,31 @@ import {
 import CreateNewPost from "../src/components/CreateNewPost/CreateNewPost";
 
 function App() {
-
-
   const [inputText, setInputText] = useState("");
   const [posts, setPosts] = useState([]);
 
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-     
-      <Switch>
-        <Route exact path='/'>
-        {posts.map((post) =>(<PostText  text={post.text}/> ))}
-        </Route>
-        <Route path='/create'>
-          <CreateNewPost posts={posts} setPosts={setPosts} inputText={inputText} setInputText={setInputText} />
-        </Route>
-      </Switch>
-    </div>
+      <div className="App">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/">
+            {posts.map((post) => (
+              <PostText text={post.text} />
+            ))}
+
+          </Route>
+          <Route path="/create">
+            <CreateNewPost
+              posts={posts}
+              setPosts={setPosts}
+              inputText={inputText}
+              setInputText={setInputText}
+            />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "../Navbar/Navbar.css";
 import { useHistory } from 'react-router-dom';
+import { useEthers, useEtherBalance } from "@usedapp/core";
 
 const Navbar = () => {
 
   const history = useHistory();
+  const { activateBrowserWallet, account } = useEthers()
 
   return (
     <div>
       <div className='navbar'>
 
-          <button className='navbar-button animation' onClick={() => history.push('/create')}>Create</button>
+          <button className='navbarButtonLeft animation' onClick={() => history.push('/create')}>Create</button>
           <button className='logo' onClick={() => history.push('/')}>The Content Protocol</button>
+          {/* ConnetWalletButton */}
+          <div>
+          <span>{account && <p>Account: {account}</p>}</span>
+        <button className='navbarButtonRight animation'  onClick={() => activateBrowserWallet()}>Log In</button>
+      </div>
       </div>
     </div>
   );
