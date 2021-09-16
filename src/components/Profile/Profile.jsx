3829a010630148698207cf392c9e3ea12bbdbf40
chet2chet <karolproksa@gmail.com>
2021-09-16 20:45:10 +0200

    Added log out button.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from '../Profile/Profile.module.css'
+import { useEthers } from "@usedapp/core";
 
 const Profile = ({username, walletAddress}) => {
+
+    const { deactivate } = useEthers();
 
     return (
         <>
@@ -9,7 +12,7 @@ const Profile = ({username, walletAddress}) => {
                 <div className={styles.profilePicture}></div>
                 <div className={styles.username}>{username}</div>
                 <div className={styles.walletAddress}>{walletAddress}</div>
-                <button className={styles.logOutButton}>Log Out</button>
+                <button className={[styles.logOutButton, styles.animation].join(" ")} onClick={() => deactivate()}>Log Out</button>
 
             </div>
         </>
