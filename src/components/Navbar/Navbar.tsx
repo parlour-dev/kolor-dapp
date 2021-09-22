5bcdbb3236b398b5ce6dd82a09ad5db62dcbc5d4
chet2chet <karolproksa@gmail.com>
2021-09-22 21:14:23 +0200

    Showing balance in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,20 +2,32 @@ import React from "react";
 import styles from "../Navbar/Navbar.module.css";
 import { useHistory } from "react-router-dom";
 import { useEthers } from "@usedapp/core";
+import { useEtherBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
 
 const Navbar = () => {
 	const history = useHistory();
 	const { activateBrowserWallet, account } = useEthers();
+	const etherBalance = useEtherBalance(account);
 
 	return (
 		<div>
 			<div className={styles.navbar}>
-				<button
-					className={[styles.navbarButtonLeft, styles.animation].join(" ")}
-					onClick={() => history.push("/create")}
-				>
-					Create
-				</button>
+				<div>
+					<button
+						className={[styles.navbarButtonLeft, styles.animation].join(" ")}
+						onClick={() => history.push("/create")}
+					>
+						Create
+					</button>
+					<div>
+						{etherBalance && (
+							<div className={styles.balance}>
+								Balance: <b>{formatEther(etherBalance).substr(0, 8)} ETH</b>
+							</div>
+						)}
+					</div>
+				</div>
 				<button className={styles.logo} onClick={() => history.push("/")}>
 					The Content Protocol
 				</button>
