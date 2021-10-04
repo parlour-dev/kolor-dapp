60b3e6a573c1a6f4b43ec9008b95ec8f40e2452c
chet2chet <karolproksa@gmail.com>
2021-10-04 21:05:46 +0200

    Added an input for changing username.

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,6 +3,7 @@ import styles from "../Profile/Profile.module.css";
 import { useEthers } from "@usedapp/core";
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
 import editIcon from "../Profile/editIcon.png";
+import Popup from "reactjs-popup";
 
 type ProfileT = {
 	username: string;
@@ -13,15 +14,42 @@ type ProfileT = {
 const Profile: React.FC<ProfileT> = ({ username, walletAddress, author }) => {
 	const { deactivate } = useEthers();
 
+	const ChangeUsername = () => {
+		alert("helo");
+	};
+
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.profilePicture}>
-			<ProfilePicture address={author}/>
-			</div>
-			<div className={styles.usernameEditBox}>
-				<div className={styles.username}>{username}</div>
-					<img src={editIcon} alt="edit" className={styles.editUsername}/>
+					<ProfilePicture address={author} />
+				</div>
+				<div className={styles.usernameEditBox}>
+					<div className={styles.username}>{username}</div>
+					<Popup
+						trigger={
+							<button
+								onClick={ChangeUsername}
+								className={styles.usernameEditButton}
+							>
+								<img
+									src={editIcon}
+									alt="edit"
+									className={styles.editUsername}
+								/>
+							</button>
+						}
+						modal
+					>
+						<div className={styles.popup}>
+							<input
+								type="text"
+								className={styles.popupInput}
+								placeholder="Your beautiful name"
+							/>
+							<div className={styles.popupTip}>Change</div>
+						</div>
+					</Popup>
 				</div>
 				<div className={styles.walletAddress}>{walletAddress}</div>
 				<button
