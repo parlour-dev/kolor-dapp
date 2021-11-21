a39bc06cc3bc8a8d6bf46564ae1ae6ca3a3a1fc2
KubaMach667 <90350478+KubaMach667@users.noreply.github.com>
2021-11-21 15:40:07 +0100

    Init newpost (#41)
    * chainSelect
    
    * layout
    
    Co-authored-by: Antoni Kiszka <30828906+akiszka@users.noreply.github.com>

---
+++ src/components/CreateNewPost/ImagePost.tsx
@@ -0,0 +1,9 @@
+import styles from "./ImagePost.module.css";
+export default function ImagePost(props: any) {
+	return (
+		<div className={styles.container}>
+			<div className={styles.test1}>ad</div>
+			<div className={styles.test2}>ad</div>
+		</div>
+	);
+}
